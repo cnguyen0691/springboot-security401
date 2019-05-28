b32f8e57666e56510609b8122b4affa70ecfab52
cnguyen0691 <chaunguyen0691@gmail.com>
2019-05-28 14:11:15 -0400

    login fail message

--- src/main/java/com/example/sercurity401/SecurityConfiguration.java
+++ src/main/java/com/example/sercurity401/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin();
+                .formLogin().loginPage("/login").permitAll();
     }
         @Override
                 protected void configure(AuthenticationManagerBuilder auth) throws Exception{
