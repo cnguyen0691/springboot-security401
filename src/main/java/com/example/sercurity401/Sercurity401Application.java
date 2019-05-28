8c47a09ff6363474cef028369c815633832b32e5
cnguyen0691 <chaunguyen0691@gmail.com>
2019-05-28 10:20:16 -0400

    initial

---
+++ src/main/java/com/example/sercurity401/Sercurity401Application.java
@@ -0,0 +1,13 @@
+package com.example.sercurity401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sercurity401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sercurity401Application.class, args);
+	}
+
+}
